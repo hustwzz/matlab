7019cc8bbdac4c4f04e76acc3c122f523a13cea3
hustwzz <44807814+hustwzz@users.noreply.github.com>
2018-11-08 19:24:21 +0800

    Update weighted_Jacobi.m

--- weighted_Jacobi.m
+++ weighted_Jacobi.m
@@ -10,16 +10,6 @@ end
 end
 
 
- syms x
-y=sin(x)*log(5-x)
-y=taylor(y,x,0,'Order',7)
+ 
 
 
-plot(1:100,iter1,1:100,iter2,1:100,iter3);xlabel('i');ylabel('f(i)');
-str = '$$ k=1 $$';
-text(86,0.9,str,'Interpreter','latex','FontSize',13);
-str2 = '$$ k=3 $$';
-text(75,0.4,str2,'Interpreter','latex','FontSize',13);
-str3 = '$$ k=6 $$';
-text(55,0.23,str3,'Interpreter','latex','FontSize',13);
-title('k=1,3,6Ê±f(i)Ëæi±ä»¯')£»
